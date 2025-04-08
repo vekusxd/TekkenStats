1d99126d9bad6d41ebecec9e0230d2e762b12ed5
Emil Batmanov <batmanovemil@gmail.com>
2025-04-08 20:19:12 +0300

    get names validation adjustments

--- Backend/TekkenStats/TekkenStats.API/Features/GetNames/GetNames.Validation.cs
+++ Backend/TekkenStats/TekkenStats.API/Features/GetNames/GetNames.Validation.cs
@@ -6,7 +6,7 @@ public class GetNamesValidator : AbstractValidator<GetNamesRequest>
 {
     public GetNamesValidator()
     {
-        RuleFor(r => r.Amount).GreaterThanOrEqualTo(1).LessThanOrEqualTo(15);
-        RuleFor(r => r.StartsWith).MaximumLength(15).NotEmpty().NotNull();
+        RuleFor(r => r.Amount).GreaterThanOrEqualTo(1).LessThanOrEqualTo(50);
+        RuleFor(r => r.StartsWith).MaximumLength(30).NotEmpty().NotNull();
     }
 }
